046ac97347ed14072930338ec170fd92fb5cbdeb
MuslemRahimi <mrahimi@stocknear.com>
2025-08-06 20:45:37 +0200

    update welcome page

--- src/routes/welcome/+page.svelte
+++ src/routes/welcome/+page.svelte
@@ -2,8 +2,11 @@
   import SEO from "$lib/components/SEO.svelte";
   import confetti from "canvas-confetti";
   import { onMount } from "svelte";
+  import { page } from "$app/stores";
 
   export let data;
+
+  let tier = $page.url.searchParams.get("tier") ?? "Unlimited";
 
   const emailAddress = "support@stocknear.com";
 
@@ -48,13 +51,13 @@
     <div class="mx-auto max-w-7xl px-6 py-16 sm:py-2 md:px-8">
       <div class="text-center">
         <img
-          class="m-auto w-20 sm:w-24 rounded-full mb-5"
+          class="m-auto w-20 sm:w-24 rounded-full mb-5 mt-3"
           src="/pwa-192x192.png"
           alt="Stocknear Logo"
           loading="lazy"
         />
-        <h1 class="mt-1 text-4xl font-bold sm:text-5xl lg:text-6xl">
-          You have now unlimited Access!
+        <h1 class="mt-1 text-4xl font-bold sm:text-5xl">
+          You have now {tier} Access!
         </h1>
         <p
           class="mx-auto mt-5 max-w-3xl text-xl leading-relaxed md:mt-7 md:text-2xl mb-10"
@@ -70,9 +73,10 @@
     <h2 class="text-xl font-semibold mb-5">Hello there 👋,</h2>
 
     <p class="mb-5 text-base md:text-lg leading-relaxed">
-      Thank you for subscribing to Stocknear! As the sole creator—handling
-      everything from backend development to customer support—I’m committed to
-      keeping our platform both affordable and accessible for every investor.
+      Thank you for subscribing to <strong>Stocknear</strong>! As the sole
+      creator—handling everything from backend development to customer support.
+      I’m committed to keeping our platform both affordable and accessible for
+      every investor.
     </p>
 
     <p class="mb-5 text-base md:text-lg leading-relaxed">
@@ -100,13 +104,13 @@
     </ul>
 
     <p class="text-base md:text-lg leading-relaxed">
-      Thanks again for joining Stocknear. Expect fast, regular updates and new
-      features coming your way!
+      Thanks again for joining <strong>Stocknear</strong>. Expect fast, regular
+      updates and new features coming your way!
     </p>
 
     <a
       href="/"
-      class="flex justify-center items-center w-fit px-10 m-auto py-2.5 mt-10 rounded duration-100 bg-[#3B82F6] text-white sm:hover:bg-blue-600 dark:bg-[#fff] dark:sm:hover:hover:bg-gray-300 dark:text-black font-semibold"
+      class="flex justify-center items-center w-fit px-10 m-auto py-2.5 mt-10 rounded duration-100 bg-black text-white sm:hover:bg-default dark:bg-[#fff] dark:sm:hover:hover:bg-gray-300 dark:text-black font-semibold"
     >
       Start Researching Now!
     </a>
