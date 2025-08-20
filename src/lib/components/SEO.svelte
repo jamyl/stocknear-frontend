cde2dd2604670ac4fb0f348f1fe1a31a4253833a
MuslemRahimi <mrahimi@stocknear.com>
2025-08-20 18:00:08 +0200

    update seo

--- src/lib/components/SEO.svelte
+++ src/lib/components/SEO.svelte
@@ -67,6 +67,7 @@
   <meta property="og:url" content={canonical} />
   <meta property="og:title" content={`${title} - ${siteName}`} />
   <meta property="og:description" content={description} />
+  <!--
   {#if image}
     <meta property="og:image" content={image} />
     <meta property="og:image:width" content="256" />
@@ -74,7 +75,7 @@
     <meta property="og:image:type" content="image/png" />
     <meta property="og:image:alt" content={`${title} - ${siteName}`} />
   {/if}
-
+-->
   {#if article}
     <meta property="article:author" content={siteName} />
     <meta property="article:publisher" content={baseURL} />
@@ -100,10 +101,12 @@
   <meta name="twitter:creator" content={twitterHandle} />
   <meta name="twitter:title" content={`${title} - ${siteName}`} />
   <meta name="twitter:description" content={description} />
+  <!--
   {#if image}
     <meta name="twitter:image" content={image} />
     <meta name="twitter:image:alt" content={`${title} - ${siteName}`} />
   {/if}
+  -->
 
   <!-- Apple mobile web-app -->
   <meta name="apple-mobile-web-app-title" content={siteName} />
